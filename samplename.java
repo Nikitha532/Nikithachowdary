e21b7e8c870a789a0192c2f694c83c91d9f8bf8c
nikithachowdary <nikithachowdary532gmail.com>
2025-09-05 21:14:09 +0530

    samplename.java

--- samplename.java
+++ samplename.java
@@ -1,6 +1,6 @@
 public class Sample{
     public static void main(String[]args){
         String name="nikithachowdary";
-        System.out.println("Hello,my name is"+name);cd
+        System.out.println("Hello,my name is"+name);
     }
 }
