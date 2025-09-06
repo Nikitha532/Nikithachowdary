6c0c2c38d0e23dff5b1e0184906cebbec2777b98
kosarajunikithachowdary <Nikithachowdary532@gmail.com>
2025-09-06 11:17:51 +0530

    Update samplename.java

--- samplename.java
+++ samplename.java
@@ -2,6 +2,5 @@ public class Sample{
     public static void main(String[]args){
         String name="nikithachowdary";
         System.out.println("Hello,my name is"+name);
-        System.out.println("Hello,my name is"+name);
     }
 }
